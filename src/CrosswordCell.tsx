54717f74afb9d51251ada64c07e14468532ce7d4
Jeremy Yap <jeremy.yapjl@gmail.com>
2023-09-26 00:55:57 +0800

    Render clue numbers

--- src/CrosswordCell.tsx
+++ src/CrosswordCell.tsx
@@ -10,16 +10,37 @@ type Props = {
 
 export default function CrosswordCell({row, col, cellInfo}: Props) {
   const fill = cellInfo === '#' ? 'black' : 'white';
+  const clueNumber = getClueNumber(cellInfo);
 
   return <g>
     <rect
-      x={row}
-      y={col}
+      x={col}
+      y={row}
       width={1}
       height={1}
       fill={fill}
       stroke="black"
       strokeWidth={.01}
     />
+    {clueNumber && <text
+      x={col + .05}
+      y={row + .05}
+      dominantBaseline="hanging"
+      style={{ fontSize: '.3', fill: "black" }}
+    >
+      {clueNumber}
+    </text>}
   </g>;
+}
+
+function getClueNumber(cellInfo: Ipuz['puzzle'][number][number]): number | null {
+  if (cellInfo instanceof Object) {
+    return cellInfo.cell === 0 ? null : cellInfo.cell;
+  }
+
+  if (typeof cellInfo === "string") {
+    return null;
+  }
+
+  return cellInfo === 0 ? null : cellInfo;
 }
