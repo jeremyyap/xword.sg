694b8161774f5a9eb0d261f9df2cc4a8dd62e740
Jeremy Yap <jeremy.yapjl@gmail.com>
2023-09-26 00:39:11 +0800

    Render crossword grid

--- src/CrosswordCell.tsx
+++ src/CrosswordCell.tsx
@@ -1,3 +1,25 @@
-export default function CrosswordCell() {
-  return null;
+import { Ipuz } from "./Ipuz";
+
+type Props = {
+  row: number;
+  col: number;
+  answer: string;
+  input: string;
+  cellInfo: Ipuz['puzzle'][number][number]
+}
+
+export default function CrosswordCell({row, col, cellInfo}: Props) {
+  const fill = cellInfo === '#' ? 'black' : 'white';
+
+  return <g>
+    <rect
+      x={row}
+      y={col}
+      width={1}
+      height={1}
+      fill={fill}
+      stroke="black"
+      strokeWidth={.01}
+    />
+  </g>;
 }
--- src/CrosswordGrid.tsx
+++ src/CrosswordGrid.tsx
@@ -1,9 +1,30 @@
+import CrosswordCell from "./CrosswordCell";
 import { Ipuz } from "./Ipuz";
 
 type Props = {
     data: Ipuz;
 }
 
-export default function CrosswordGrid(props: Props) {
-    return <div>{JSON.stringify(props)}</div>
+export default function CrosswordGrid({ data }: Props) {
+  const { height, width } = data.dimensions;
+  const puzzleGrid = data.puzzle;
+  const answerGrid = data.solution;
+  const inputGrid: Array<Array<string>> = Array.from({
+    length: height
+  }, () => new Array(width).fill(''));
+  
+  return <svg viewBox={`0 0 ${width} ${height}`} height={600} width={600}>
+    {Array.from({length: width}, (_, i) => i).flatMap((row) => 
+      Array.from({length: height}, (_, i) => i).map((col) => {
+        return <CrosswordCell
+          key={`${row} ${col}`}
+          row={row}
+          col={col}
+          answer={answerGrid[row][col]}
+          input={inputGrid[row][col]}
+          cellInfo={puzzleGrid[row][col]}
+        />
+      })
+    )}
+  </svg>
 }
