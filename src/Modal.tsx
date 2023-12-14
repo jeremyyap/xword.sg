0bfe672ba37c8c875d8e780d7ebb0912d3b19b83
Jeremy Yap <jeremy.yapjl@gmail.com>
2023-12-14 21:09:51 +0800

    FFix modal bug  - any click on modal causes it to close

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -7,13 +7,14 @@ type Props = {
 
 export default function Modal({ content, onHide }: Props) {
   return (
-    <div className="modal-bg" onClick={onHide}>
+    <>
+      <div className="modal-bg" onClick={onHide} />
       <div className="modal">
         <div className="close-button" onClick={onHide}>
           &times;
         </div>
         <div>{content}</div>
       </div>
-    </div>
+    </>
   );
 }
