5085d24f0b4bcc46615b712f4857af0e2a9c9443
Jeremy Yap <jeremy.yapjl@gmail.com>
2023-12-19 00:30:02 +0800

    Add options method for CORS

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -144,6 +144,50 @@ resource "aws_api_gateway_integration_response" "post_save_response" {
   }
 }
 
+# OPTIONS /saves/{date}
+resource "aws_api_gateway_method" "options_save" {
+  rest_api_id   = aws_api_gateway_rest_api.xword_saves_api.id
+  resource_id   = aws_api_gateway_resource.save.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_save_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.xword_saves_api.id
+  resource_id             = aws_api_gateway_resource.save.id
+  http_method             = aws_api_gateway_method.options_save.http_method
+  type                    = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_save_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.xword_saves_api.id
+  resource_id = aws_api_gateway_resource.save.id
+  http_method = aws_api_gateway_method.options_save.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_save_response" {
+  depends_on = [aws_api_gateway_integration.options_save_integration]
+  rest_api_id = aws_api_gateway_rest_api.xword_saves_api.id
+  resource_id = aws_api_gateway_resource.save.id
+  http_method = aws_api_gateway_method_response.options_save_response_200.http_method
+  status_code = aws_api_gateway_method_response.options_save_response_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'*'"
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "xword_saves_api" {
   rest_api_id = aws_api_gateway_rest_api.xword_saves_api.id
 
