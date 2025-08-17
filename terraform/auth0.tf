9533d2c7f7283f99fddb9b8ff15167c35a77c6ad
Jeremy Yap <jeremy.yapjl@gmail.com>
2025-08-17 18:45:27 +0800

    Fix login not working for xword.sg domain

--- terraform/auth0.tf
+++ terraform/auth0.tf
@@ -4,10 +4,10 @@ resource "auth0_client" "xword_sg" {
   name                                = "xword.sg"
   app_type                            = "spa"
   custom_login_page_on                = true
-  callbacks                           = ["http://localhost:5173", "https://www.xword.sg"]
-  allowed_origins                     = ["http://localhost:5173", "https://www.xword.sg"]
-  allowed_logout_urls                 = ["http://localhost:5173", "https://www.xword.sg"]
-  web_origins                         = ["http://localhost:5173", "https://www.xword.sg"]
+  callbacks                           = ["http://localhost:5173", "https://www.xword.sg", "https://xword.sg"]
+  allowed_origins                     = ["http://localhost:5173", "https://www.xword.sg", "https://xword.sg"]
+  allowed_logout_urls                 = ["http://localhost:5173", "https://www.xword.sg", "https://xword.sg"]
+  web_origins                         = ["http://localhost:5173", "https://www.xword.sg", "https://xword.sg"]
 }
 
 resource "auth0_resource_server" "api_xword_sg" {
